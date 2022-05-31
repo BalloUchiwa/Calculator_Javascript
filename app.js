a250ec515f6dc22505581e05e6f699db648b4ec5
BalloUchiwa <ballomohamadou1@gmail.com>
2022-05-31 07:39:56 +0100

    Edit app.js

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const buttons = Array.from(document.getElementsByClassName('button'))
 
 buttons.map(buttonClick => {buttonClick.addEventListener('click',(e) => {
   switch(e.target.innerText){
+    case 'C':
+        display.innerText = ''
+        break
     
+    default:
+        display.innerText += e.target.innerText
   }
 })})
