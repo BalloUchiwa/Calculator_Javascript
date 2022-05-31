7efaa2e87b18b2dbc6b26d3bd8fcb7fe03f38609
BalloUchiwa <ballomohamadou1@gmail.com>
2022-05-31 07:34:11 +0100

    Edit app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const display = document.getElementById('display')
+const buttons = Array.from(document.getElementsByClassName('button'))
+
+buttons.map(buttonClick => {buttonClick.addEventListener('click',(e) => {
+  switch(e.target.innerText){
+    
+  }
+})})
